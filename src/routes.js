bdabf5b2befd79e0e3d3072f4d242515f242f91c
Henrique_Gaeta <henriquehg@gmail.com>
2021-05-17 20:51:17 -0300

    add game admin route

--- src/routes.js
+++ src/routes.js
@@ -10,4 +10,7 @@ routes.get('/fruitGame', function(req, res) {
   res.send('/public/fruitGame/', { root: '.' })
 })
 
+routes.get('/a31ecc0596d72f84e5ee403ddcacb3dea94ce0803fc9e6dc2eca1', function(req, res) {
+  res.send('/public/fruitGame/game-admin.html', { root: '.' })
+})
 export default routes
